93d50bbdb50532d737d99e47e5d3f7fdc0a8243a
yushu <yushu@codemao.cn>
2026-06-22 17:00:19 +0800

    Fix manifest icon purpose type

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -17,7 +17,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: `${basePath}/icon.svg`,
         sizes: "any",
         type: "image/svg+xml",
-        purpose: "any maskable",
+        purpose: "any",
       },
     ],
   };
